93cf117c7a92a8f257e1c320aaa6a9f6d19bf76a
SungHong-Cho <lunaticbreeze@gmail.com>
2013-08-28 20:35:42 +0900

    Deployment config

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -3,10 +3,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/ubuntu/Projects/ccl_muse
+APP_ROOT=/home/deployer/apps/boa_incognito/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb"
-AS_USER=andy
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+AS_USER=deployer
 set -u
 
 OLD_PIN="$PID.oldbin"
